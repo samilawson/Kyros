2d8570afd77c8305ecfb9133bccd01b005d7870f
Sam Lawson <samilawson17@gmail.com>
2019-02-25 11:24:10 -0600

    remove unneeded package

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -1,5 +1,4 @@
 const { Command } = require("discord.js-commando");
-const { RichEmbed } = require("discord.js");
 
 module.exports = class CoinflipCommand extends Command {
   constructor(client) {
