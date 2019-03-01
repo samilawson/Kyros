96c084d677338407f78c7a66c552f28dc1a504a3
Sam Lawson <samilawson17@gmail.com>
2019-03-01 15:22:02 -0600

    remove uneeded package

--- commands/util/wikipedia.js
+++ commands/util/wikipedia.js
@@ -1,5 +1,4 @@
 const { Command } = require("discord.js-commando");
-const { RichEmbed } = require("discord.js");
 const request = require("superagent");
 module.exports = class WikipediaCommand extends Command {
   constructor(client) {
